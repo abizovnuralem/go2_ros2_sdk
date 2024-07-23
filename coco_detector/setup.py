dd991e9f546eb2fd1b216efbad6f0044ed9b0ca8
TMC <TangmereCottage@protonmail.com>
2024-07-23 16:02:11 -0700

    trying to fix build

--- coco_detector/setup.py
+++ coco_detector/setup.py
@@ -1,11 +1,14 @@
+import os
+from glob import glob
 from setuptools import setup
+from setuptools import find_packages
 
 package_name = 'coco_detector'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
