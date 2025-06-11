a1d639bf0030081d60508a0f96e985f5f1272c07
Juergen Hasch <juergen.hasch@mmwaves.de>
2025-06-11 19:48:50 +0200

    Add patch for aioice and aiortc

--- go2_robot_sdk/scripts/webrtc_driver.py
+++ go2_robot_sdk/scripts/webrtc_driver.py
@@ -39,6 +39,48 @@ from Crypto.Cipher import PKCS1_v1_5
 import requests
 from aiortc import RTCPeerConnection, RTCSessionDescription
 
+# Monkey-patch aioice.Connection to use a fixed username and password accross all instances.
+
+import aioice
+
+
+class Connection(aioice.Connection):
+    local_username = aioice.utils.random_string(4)
+    local_password = aioice.utils.random_string(22)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.local_username = Connection.local_username
+        self.local_password = Connection.local_password
+
+
+aioice.Connection = Connection  # type: ignore
+
+
+# Monkey-patch aiortc.rtcdtlstransport.X509_DIGEST_ALGORITHMS to remove extra SHA algorithms
+# Extra SHA algorithms introduced in aiortc 1.10.0 causes Unity Go2 to use the new SCTP format, despite aiortc using the old SCTP syntax.
+# This new format is not supported by aiortc version as of today (2025-06-02)
+
+
+import aiortc
+from packaging.version import Version
+
+
+if Version(aiortc.__version__) == Version("1.10.0"):
+    X509_DIGEST_ALGORITHMS = {
+        "sha-256": "SHA256",
+    }
+    aiortc.rtcdtlstransport.X509_DIGEST_ALGORITHMS = X509_DIGEST_ALGORITHMS
+
+elif Version(aiortc.__version__) >= Version("1.11.0"):
+    # Syntax changed in aiortc 1.11.0, so we need to use the hashes module
+    from cryptography.hazmat.primitives import hashes
+
+    X509_DIGEST_ALGORITHMS = {
+        "sha-256": hashes.SHA256(),  # type: ignore
+    }
+    aiortc.rtcdtlstransport.X509_DIGEST_ALGORITHMS = X509_DIGEST_ALGORITHMS
+
 
 from scripts.go2_lidar_decoder import LidarDecoder
 
