9b047157fe444a5cc84ee90cacb54643d4b8fe1c
Jürgen Hasch <juhasch@mmwaves.de>
2025-06-12 16:31:11 +0200

    Remove custom aioice

--- go2_robot_sdk/go2_robot_sdk/__init__.py
+++ go2_robot_sdk/go2_robot_sdk/__init__.py
@@ -20,24 +20,3 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-import sys
-import os
-import logging
-from ament_index_python import get_package_share_directory
-
-logger = logging.getLogger(__name__)
-
-libs_path = os.path.join(
-    get_package_share_directory('go2_robot_sdk'),
-    'external_lib'
-)
-
-if os.path.exists(os.path.join(libs_path, 'aioice', '__init__.py')):
-    sys.path.insert(0, os.path.join(libs_path, 'aioice'))
-    sys.path.insert(0, os.path.join(libs_path))
-
-    logger.info('Patched aioice added to sys.path: {}'.format(sys.path))
-else:
-    logger.error("aioice submodule is not initalized. please init submodules recursively")
-    exit(-1)
